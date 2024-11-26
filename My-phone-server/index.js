b8a04b62e9e289b19edc4c235f036fbcf4cead0b
Abdallah-BinSaif <binsaif.info@gmail.com>
2024-11-26 08:40:15 +0600

    hello world project and second project phones projec

---
+++ My-phone-server/index.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const phones = require("./phones.json")
+const app = express()
+const port = 3344
+
+app.get("/", (req,res)=>{
+    res.send("Hello World,the new world")
+})
+
+app.get("/phones", (req, res)=>{
+    res.send(phones)
+})
+
+app.get("/phones/:id", (req, res)=>{
+    const id = parseInt(req.params.id);
+    console.log("i need data for :", id)
+    const phone = phones.find(phone=>phone.id === id) || {};
+    res.send(phone);
+})
+
+app.listen(port,()=>{
+    console.log(`Example project is running on port: ${port}`)
+})
